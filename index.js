ecf0f5060ddc6d2c9b965c3ff405eadf1eb498e9
Hugo Sanchez <hugosanchez251@gmail.com>
2018-05-05 18:26:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,3 +20,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object;
 }
 
+function deleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
+}
